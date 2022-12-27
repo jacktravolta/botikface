41b1e5561c0dd8f7306903cff653e72d97a6207f
jacktravolta <63665298+jacktravolta@users.noreply.github.com>
2022-12-27 17:20:29 -0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo apt udpate -y
+sudo apt install python3-pip -y
+#sudo apt install python3-tk python3-dev -y
+apt-get install python3-opencv -y
+apt install scrot -y
+apt install xinit -y
+add-apt-repositorio ppa:mozillateam/ppa -y
+
+apt install firefox-esr -y
+apt install cmake -y
+apt-get install libjpeg-dev zlib1g-dev -y
+apt install xfce4 -y
