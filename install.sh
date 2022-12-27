43f3ccf6ff4d5814640907704ebb9b41cc435321
jacktravolta <63665298+jacktravolta@users.noreply.github.com>
2022-12-27 18:00:39 -0300

    Update install.sh
    se agregan dependencias necesarias

--- install.sh
+++ install.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
+
 sudo apt udpate -y
 sudo apt install python3-pip -y
-#sudo apt install python3-tk python3-dev -y
-apt-get install python3-opencv -y
-apt install scrot -y
-apt install xinit -y
-add-apt-repositorio ppa:mozillateam/ppa -y
+sudo apt-get install python3-opencv -y
+sudo apt install scrot -y
+sudo apt install xinit -y
+sudo add-apt-repositorio ppa:mozillateam/ppa -y
 
-apt install firefox-esr -y
-apt install cmake -y
-apt-get install libjpeg-dev zlib1g-dev -y
-apt install xfce4 -y
+sudo apt install firefox-esr -y
+sudo apt install cmake -y
+sudo apt-get install libjpeg-dev zlib1g-dev -y
+sudo apt-get install python3-tk python3-dev -y
+
+pip3 install pyautogui
+pip3 install opencv-python
+pip3 install Pillow
